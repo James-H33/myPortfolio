9a8f8011fd90ae3f5d6e54ba5ca9da9c66d4ddc1
James Hall <llahwj33@gmail.com>
2016-04-10 10:49:43 -0400

    Initialize

---
+++ assets/js/functions.js
@@ -0,0 +1,67 @@
+$(document).ready(function() {
+    
+    // Reveals information on hover about projects
+    $('.index-project-display div').on('mouseover', function() {
+        $(this).addClass('reveal'); 
+    });
+
+    $('.index-project-display div').on('mouseout', function() {
+        $(this).removeClass('reveal'); 
+    });
+    
+    
+    // Nav menu transform and display
+    $('#nav-icon').on('click', function() {
+        
+         $('.menu-overlay').toggleClass('open-menu');
+         $('.menu-wrapper').toggleClass('open-menu');
+         $('#nav-icon span').toggleClass('open');
+    });
+    
+    
+        // Page Scroll Event
+    $('.index-page-scroll').click(function() {
+        $('html,body').animate({
+            scrollTop: $('.index-project-wrapper').offset().top},
+            1000);
+    });
+    
+        // Page Scroll Event
+    $('.about-page-scroll').click(function() {
+        $('html,body').animate({
+            scrollTop: $('.about-info-wrapper').offset().top},
+            1000);
+    });
+    
+    
+    // slide In Sections
+    $(window).on('scroll', function() {
+        var scrollCheck = $(window).scrollTop();
+        
+        // if(scrollCheck > $('.about-info-wrapper').offset().top - ($(window).height() / 1.2)) {
+        //     $('.about-info-wrapper').css({
+        //        'transform' : 'translateY(0)',
+        //        'opacity' : 1 
+        //     });
+        // }
+        
+        if(scrollCheck > $('.about-lang-wrapper').offset().top - ($(window).height() / 1.2)) {
+            $('.about-lang-wrapper').css({
+               'transform' : 'translateY(0)',
+               'opacity' : 1 
+            });
+        }
+        
+        
+    }); 
+
+});
+
+
+
+
+
+
+
+
+
---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+var gulp         = require('gulp');
+var uglify       = require('gulp-uglify');
+var sass         = require('gulp-ruby-sass');
+var plumber      = require('gulp-plumber');
+var autoprefixer = require('gulp-autoprefixer');
+var browserSync  = require('browser-sync').create();
+
+// Static server
+gulp.task('serve', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+    
+    gulp.watch("assets/scss/**/*.scss", ['sass']);
+    gulp.watch("assets/js/*.js").on('change', browserSync.reload);
+    gulp.watch("./**/*.html").on('change', browserSync.reload);
+});
+
+
+// Compiles SASS and Adds browser support
+gulp.task('sass', function() {
+    return sass('assets/scss/main.scss')
+        .on('error', sass.logError)
+        .pipe(plumber())
+        .pipe(autoprefixer({
+                browser: ['last 2 versions'],
+                cascade: false
+        }))
+        .pipe(gulp.dest('assets/css'))
+        .pipe(browserSync.reload({stream: true}));
+});
+
+// Default Task -- Excutes all other task in order
+gulp.task('default', ['sass', 'serve']);
